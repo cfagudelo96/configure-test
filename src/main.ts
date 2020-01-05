261c585c9a3f98c6f71681707f9b60377459f62e
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2020-01-05 13:49:46 -0500

    Add security measures to the webapp

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 
 import { AppModule } from './app.module';
 
+const RATE_LIMIT_WINDOW = 15 * 60 * 1000;
+const MAX_REQUESTS_PER_WINDOW = 100;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
+  app.use(
+    rateLimit({
+      windowMs: RATE_LIMIT_WINDOW,
+      max: MAX_REQUESTS_PER_WINDOW
+    })
+  );
   await app.listen(3000);
 }
 bootstrap();
